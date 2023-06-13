b692cdf01a0dd9b4d28fe31893504c8d20d58295
Cristian Vieira <cristian.evangelista@fatec.sp.gov.br>
2023-06-12 22:15:06 -0300

    Created new past
    Created past: Exemplo de como usar metodos de array map() e filter()

---
+++ Exemplo de como usar metodos de array map() e filter()/Função que usa map() e filter() para somar numeros pares passados como parametros.js
@@ -0,0 +1,16 @@
+//Função criada para somar e retornar somente os numeros pares passados como parâmetros 
+function somarPares(...nums) {
+    let sum = 0; //Variavel para armazenar o valor da soma
+  
+    let pares = nums.filter(x => x % 2 == 0); // Metodo filter cria uma nova array com apenas os numeros pares que vieram como parâmetros
+    pares.map(numsFiltrados => sum += numsFiltrados); // Metodo map() pega todos os elementos da array "pares" e soma com a variavel "sum"
+  
+    /*
+    As 2 linhas a cima poderiam ser simplificadas para:
+    nums.filter(x => x % 2 == 0).map(numsFiltrados => sum += numsFiltrados)
+    */
+  
+    return `A soma dos numeros pares ${pares} é ${sum}`; // Retornamos a array pares e a soma dos elementos dela
+  };
+  
+  console.log(somarPares(1, 2, 3, 4, 5, 6)); // A soma dos numeros pares 2,4,6 é 12
