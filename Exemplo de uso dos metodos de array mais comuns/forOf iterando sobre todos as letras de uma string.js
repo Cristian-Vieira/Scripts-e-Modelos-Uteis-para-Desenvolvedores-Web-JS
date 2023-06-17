a963683089c2abdeef8f767206e17f08305ba914
Cristian Vieira <cristian.evangelista@fatec.sp.gov.br>
2023-06-16 22:59:47 -0300

    Update forOf iterando sobre todos as letras de uma string.js

---
+++ Exemplo de uso dos metodos de array mais comuns/forOf iterando sobre todos as letras de uma string.js
@@ -0,0 +1,7 @@
+let string = "abcdffghijklmnopqrstwxyz"
+
+for (let letras of string ) { // A cada iteração a variavel letra recebe o valor do elemento da iterção
+    console.log(letras); // retorna todas as letras da variavel string uma em cada linha
+}
+
+// laço for of pode iterar sobre qualquer ojeto iteravel
