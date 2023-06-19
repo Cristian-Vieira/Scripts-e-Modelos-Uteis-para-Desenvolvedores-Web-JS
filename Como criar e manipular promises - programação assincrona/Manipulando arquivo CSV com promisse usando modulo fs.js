2640a5d7afbd1d1999e3419297583879312c47b7
Cristian Vieira <cristian.evangelista@fatec.sp.gov.br>
2023-06-19 00:08:04 -0300

    Update Manipulando arquivo CSV com promisse usando modulo fs.js
    Add coments

--- Como criar e manipular promises - programação assincrona/Manipulando arquivo CSV com promisse usando modulo fs.js
+++ Como criar e manipular promises - programação assincrona/Manipulando arquivo CSV com promisse usando modulo fs.js
@@ -1,26 +1,26 @@
 // Usando promise para manipular arquivos
-const fs = require('fs'); // Modulo de leitura de arquivos
+const fs = require('fs') // Modulo de leitura de arquivos
 const path = require('path') // Modulo para manipular arquivos e diretórios
 
 const filePath = path.resolve(__dirname, 'exemploListaTarefas.csv')  // Caminho para o arquivo
-const promessaDaLeituraDoArquivo = fs.promises.readFile(filePath);  // Criando promise para ler o arquivo
+const promessaDaLeituraDoArquivo = fs.promises.readFile(filePath)  // Criando promise para ler o arquivo
 
 promessaDaLeituraDoArquivo.then((arquivo) => {
      // Primeiro convertemos o arquivo para string no formato utf-8, depois substituimos os valores booleanos para strings mais descritivas
-    return arquivo.toString("utf-8").replace(/true/g, "Feita").replace(/false/g, "Não feita");
+    return arquivo.toString("utf-8").replace(/true/g, "Feita").replace(/false/g, "Não feita")
 
 }).then((arquivo) => {
      // Convertemos o retorno para uma array, separando os elementos por ","
-    return arquivo.split(",");
+    return arquivo.split(",")
 
 }).then((array) => {
 
      // Criamos uma nova array e a cada elemento da array que veio como retorno, unimos em um elemento da nova array
-    const newArr = []; 
+    const newArr = [] 
     let index = 0
     for (let i = 0; i < array.length - 1; i += 2) {
-        let element = array[i];
-        let element2 = array[i + 1];
+        let element = array[i]
+        let element2 = array[i + 1]
         newArr[index] = element + " >" + element2
         index++
     }
@@ -31,8 +31,7 @@ promessaDaLeituraDoArquivo.then((arquivo) => {
     for (const elementos of array) {
         console.log(elementos)
     }
-
-});
+})
 console.log("\nLista de tarefas:\n")
 
 /*
