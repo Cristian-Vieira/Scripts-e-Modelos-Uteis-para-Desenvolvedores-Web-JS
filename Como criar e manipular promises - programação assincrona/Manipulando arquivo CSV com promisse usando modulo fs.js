8998a956da660329660f69912b6118e69a39014c
Cristian Vieira <cristian.evangelista@fatec.sp.gov.br>
2023-06-18 23:59:54 -0300

    Update Manipulando arquivo CSV com promisse usando modulo fs.js

--- Como criar e manipular promises - programação assincrona/Manipulando arquivo CSV com promisse usando modulo fs.js
+++ Como criar e manipular promises - programação assincrona/Manipulando arquivo CSV com promisse usando modulo fs.js
@@ -1,21 +1,24 @@
 // Usando promise para manipular arquivos
 const fs = require('fs'); // Modulo de leitura de arquivos
-const path = require('path')
+const path = require('path') // Modulo para manipular arquivos e diretórios
 
 const filePath = path.resolve(__dirname, 'exemploListaTarefas.csv')
 const promessaDaLeituraDoArquivo = fs.promises.readFile(filePath);
 
 promessaDaLeituraDoArquivo.then((arquivo) => {
-
+     // Prieiro convertemos o arquivo para string, depois substituimos os valores booleanos para strings mais descritivas
     return arquivo.toString("utf-8").replace(/true/g, "Feita").replace(/false/g, "Não feita");
 
 }).then((arquivo) => {
-
+     // Convertemos o retorno para uma array, separando os elementos por ","
     return arquivo.split(",");
 
 }).then((array) => {
-    const newArr = [];
+
+     // Criamos uma nova array e a cada elemento da array que veio como retorno unimos em um elemento da nova array
+    const newArr = []; 
     let index = 0
+     
     for (let i = 0; i < array.length - 1; i += 2) {
         let element = array[i];
         let element2 = array[i + 1];
@@ -26,6 +29,7 @@ promessaDaLeituraDoArquivo.then((arquivo) => {
     }
     return newArr
 }).then((array) => {
+     //exibimos os elementos no console 1 em cada linha
     console.log("Lista de tarefas: \n")
     for (const item of array) {
          console.log(item)
