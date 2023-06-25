efbf983b7a6ea35f2b230b7d15c93924996df847
Cristian Vieira <cristian.evangelista@fatec.sp.gov.br>
2023-06-24 22:10:38 -0300

    Update mainScript.js

--- Como criar tootip/scripts/mainScript.js
+++ Como criar tootip/scripts/mainScript.js
@@ -1,6 +1,7 @@
   // Inicializar tooltip
   tippy('#divTooltip', {
     content: 'Texto do Tooltip, você pode altera-lo pelo script!',
+    animation: 'perspective',
   });
 
-  //More options and info in: https://atomiks.github.io/tippyjs/v6/getting-started/
+  // More options and info in: https://atomiks.github.io/tippyjs/v6/getting-started/
