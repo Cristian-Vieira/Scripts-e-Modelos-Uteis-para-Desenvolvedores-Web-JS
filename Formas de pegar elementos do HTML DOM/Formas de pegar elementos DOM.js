38ee5cecb60b082c0823c03c23f7fbd3a76c0e30
Cristian Vieira <cristian.evangelista@fatec.sp.gov.br>
2023-06-27 18:40:11 -0300

    Update Formas de pegar elementos DOM.js

--- Formas de pegar elementos do HTML DOM/Formas de pegar elementos DOM.js
+++ Formas de pegar elementos do HTML DOM/Formas de pegar elementos DOM.js
@@ -1,5 +1,5 @@
  // Seleciona o elemento por id
- document.getElementById("id")
+document.getElementById("id")
 
  // Seleciona o elemento pelo nome da class
 document.getElementsByClassName("name")
@@ -10,7 +10,7 @@ document.getElementsByTagName("name")
  // Com o querySelector pode selecionar todas as formas a cima usando a sintaxe do CSS
 document.querySelector("#meuId")
 
-// Caso deseje selecionar um elemento especifico de uma tag ou class
+ // Caso deseje selecionar um elemento especifico de uma tag ou class
 document.querySelector('div:nth-child(1)'); 
 
  // O querySelector tambem pode selecionar todos os elementos de uma classe
