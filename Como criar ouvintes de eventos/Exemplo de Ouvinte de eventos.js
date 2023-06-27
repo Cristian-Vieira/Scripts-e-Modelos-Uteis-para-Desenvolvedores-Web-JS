dab93cd7e9034538f7ffe4cf07f94fbd298b9015
Cristian Vieira <cristian.evangelista@fatec.sp.gov.br>
2023-06-27 18:32:08 -0300

    Update Exemplo de Ouvinte de eventos.js

--- Como criar ouvintes de eventos/Exemplo de Ouvinte de eventos.js
+++ Como criar ouvintes de eventos/Exemplo de Ouvinte de eventos.js
@@ -3,6 +3,7 @@ document.getElementById("ElementoQueQueremosOuvir").addEventListener(evento, fun
 /* parametro 'useCapture' é opcional e é usado para definir como sera a propagação de eventos por Capture ou Bubbling
 A Capture desce pelo DOM.
 A Bubbling sobe pelo DOM.
+O valor deve ser um boolean por padrão é 'false', o que significa que a propagação de Bubbling é usada caso o paramtro seja omitido; 
 */
 
 
